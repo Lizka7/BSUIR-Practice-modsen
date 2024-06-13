8601591629cb4007b59ff08158b5539595902e93
Lizka7 <128884829+Lizka7@users.noreply.github.com>
2024-06-13 20:17:51 +0300

    Add files via upload

---
+++ JS/use_strict_1.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function split_Words(str){
+    let result=str.split(" ")
+    return result;
+}
+
+let str="Everything happens for a reason";
+
+let result= split_Words(str);
+
+console.log(str);
+console.log(result);
